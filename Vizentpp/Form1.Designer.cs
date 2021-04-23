34adbf85d7c4fd4c7732e7e09b085d62380d1e2f
kenan durmus <kenandurmus715@gmail.com>
2021-04-24 01:52:13 +0300

    Proje dosyası ekle.

---
+++ Vizentpp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Vizentpp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clili = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clDurum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clMaksimumsicaklik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clili,
+            this.clDurum,
+            this.clMaksimumsicaklik});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(791, 365);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // clili
+            // 
+            this.clili.HeaderText = "ili";
+            this.clili.Name = "clili";
+            // 
+            // clDurum
+            // 
+            this.clDurum.HeaderText = "Durum";
+            this.clDurum.Name = "clDurum";
+            // 
+            // clMaksimumsicaklik
+            // 
+            this.clMaksimumsicaklik.HeaderText = "Maksimum Sıcaklık";
+            this.clMaksimumsicaklik.Name = "clMaksimumsicaklik";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clili;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clDurum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clMaksimumsicaklik;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Vizentpp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Vizentpp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+
+        //    XmlDocument doc1 = new XmlDocument();
+        //    doc1.Load(hava_durumu_link);
+        //    XmlElement root = doc1.DocumentElement;
+        //    XmlNodeList nodes = root.SelectNodes("sehirler");
+
+        //    foreach (XmlNode node in nodes)
+        //    {
+        //        string ili = node["ili"].InnerText;
+        //        string durum = node["Durum"].InnerText;
+        //        string maks_sicaklik = node["Mak"].InnerText;
+        //        DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+        //        row.Cells[0].Value = ili;
+        //        row.Cells[1].Value = durum;
+        //        row.Cells[2].Value = maks_sicaklik;
+        //        dataGridView1.Rows.Add(row);
+        //    }
+        //}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            String hava_durumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
+            
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(hava_durumu_link);
+            XmlElement root = doc1.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("sehirler");
+
+            foreach (XmlNode node in nodes)
+            {
+                string ili = node["ili"].InnerText;
+                string durum = node["Durum"].InnerText;
+                string maks_sicaklik = node["Mak"].InnerText;
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = ili;
+                row.Cells[1].Value = durum;
+                row.Cells[2].Value = maks_sicaklik;
+                dataGridView1.Rows.Add(row);
+                
+
+            }
+
+        }
+    }
+}
